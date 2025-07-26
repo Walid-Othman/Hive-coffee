4e4cc26e556c9d057d27d16f63cdee95f2bc8cff
Walid-Othman <waleedosman20111996@gmail.com>
2025-07-26 21:29:20 +0300

    add html caa and js

---
+++ javascript/main.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+const typedTwo = new Typed('.typed-c-two', {
+  strings: ['Coming soon with the finest coffee flavors.........'],
+   typeSpeed: 60,
+  backSpeed: 30,
+  backDelay: 2000,
+  startDelay: 500,
+  loop:Infinity
+});
+
+const typedThree = new Typed('.typed-c-three', {
+  strings: ['انتظرونا قريبًا، حيث يلتقي المزاج الراقي بالقهوة.','Stay tuned – where refined mood meets rich coffee.','Coming Soon'],
+   typeSpeed: 60,
+  backSpeed: 30,
+  backDelay: 2000,
+  startDelay: 500,
+  loop:Infinity
+});
+const typedcomingsoon = new Typed('.coming-soon', {
+  strings: ['انتظرونا قريبًا، حيث يلتقي المزاج الراقي بالقهوة','Stay tuned – where refined mood meets rich coffee.','Coming Soon'],
+   typeSpeed: 60,
+  backSpeed: 30,
+  backDelay: 2000,
+  startDelay: 500,
+  loop:Infinity
+});
+
+
